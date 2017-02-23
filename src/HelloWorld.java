06feae62ad55e9eb19a1c525ce673c72d1c178b4
Yorkyou <youyaoyu@hotmail.com>
2017-02-23 16:27:18 +0800

    demo

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Created by york on 8/23/15.
+ */
+import java.util.Scanner;
+import java.util.Random;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("104062134");
+    }
+    public String toString()
+    {
+        return String.format("fdsaf");
+    }
+}
---
+++ src/apple.java
@@ -0,0 +1,7 @@
+/**
+ * Created by york on 2017/2/14.
+ */
+import java.util.Random;
+public class apple {
+
+}
